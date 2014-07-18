f2ecf0c956a099ae81450e272c7a2ca0576f4482
Josh Sennett <josh.sennett@snagajob.com>
2014-07-18 18:51:19 -0400

    Add viewport tag

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -4,6 +4,7 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="language" content="en" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
     <!-- blueprint CSS framework -->
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />

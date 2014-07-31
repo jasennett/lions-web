9fd8fc8ff5b9581932119eead1b87592decda925
Josh Sennett <josh.sennett@snagajob.com>
2014-07-30 22:42:08 -0400

    Make new players text a link on home page

--- application/views/site/index.php
+++ application/views/site/index.php
@@ -34,7 +34,7 @@ $this->pageTitle='Richmond Lions Rugby Football Club'
                     Located in historic Richmond, Virginia, USA, the Richmond Lions R.F.C. is the area's competitive Men's Rugby Club (Division II).
                 </h2>
                 <div class="new-members" data-anchor-target=".about-text" data-80-top-top="color: rgba(255,255,255,1)" data-0-top-bottom="color: rgba(255,255,255,0)">
-                    New members are always welcome.
+                    <a href="<?php echo $this->createUrl('site/join') ?>">New members</a> are always welcome.
                 </div>
             </div>
         </div>

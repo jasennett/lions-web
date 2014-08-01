dffb6e76b6f42f4d1fb05ae33fbbf204be966402
Josh Sennett <josh.sennett@snagajob.com>
2014-08-01 00:13:05 -0400

    Sponsors page detail text

--- application/views/site/sponsors.php
+++ application/views/site/sponsors.php
@@ -9,7 +9,7 @@ $advancedLink = '//advancedortho.me';
 $gusLink = '//gusbarandgrill.com';
 ?>
 
-<section class="first light-grey">
+<section class="first light light-grey">
     <div class="content">
         <div class="gutter">
             <h2>Thanks to our sponsors!</h2>
@@ -20,10 +20,13 @@ $gusLink = '//gusbarandgrill.com';
     </div>
 </section>
 
-<section class="amazon cover dark">
+<section class="amazon sponsor cover dark">
     <div class="content">
         <div class="text-container">
             <h3><a href="<?php echo $amazonLink ?>">Amazon Smile</a></h3>
+            <p>
+                Begin your shopping experience by <a href="<?php echo $amazonLink ?>">clicking here</a> and Amazon will give up to 20% of what you spend back to Richmond rugby!
+            </p>
         </div>
         <div class="card-container">
             <a href="<?php echo $amazonLink ?>">
@@ -40,10 +43,14 @@ $gusLink = '//gusbarandgrill.com';
     </div>
 </section>
 
-<section class="impact light-grey light">
+<section class="impact sponsor light-grey light">
     <div class="content">
         <div class="text-container">
             <h3><a href="<?php echo $impactLink ?>">Impact Insurance</a></h3>
+            <p>
+                Save money on insurance while helping Richmond rugby! Tell Impact Insurance we referred you in our <a href="<?php echo $impactLink ?>">web form</a>!
+                <a href="http://www.makevirginiabetter.com">Impact Insurance Agency</a> is an independent insurance agency located in Richmond, VA. They represent several insurance carriers which enables them to find the best coverage for you at the right price.
+            </p>
         </div>
         <div class="card-container">
             <a href="<?php echo $impactLink ?>">
@@ -60,10 +67,13 @@ $gusLink = '//gusbarandgrill.com';
     </div>
 </section>
 
-<section class="pbr cover dark">
+<section class="pbr sponsor cover dark">
     <div class="content">
         <div class="text-container">
             <h3><a href="<?php echo $pbrLink ?>">Pabst Blue Ribbon</a></h3>
+            <p>
+                Pabst Blue Ribbon is a satisfying American classic beer. There's simply no wrong occasion to crack one open! Drink responsibly.
+            </p>
         </div>
         <div class="card-container">
             <a href="<?php echo $pbrLink ?>">
@@ -80,11 +90,14 @@ $gusLink = '//gusbarandgrill.com';
     </div>
 </section>
 
-<section class="gus cover dark">
+<section class="gus sponsor cover dark">
     <div class="shade">
         <div class="content">
             <div class="text-container">
                 <h3><a href="<?php echo $gusLink ?>">Gus' Bar & Grill</a></h3>
+                <p>
+                    Gus’ Bar & Grill is Richmond’s best sports bar. Located on the edge of the Fan Gus' prides themselves on their ability to combine outstanding food, cold beer, and friendly service. Stop by anytime and take advantage of their many daily specials or check out the game with friends on one their 24 plasma screen HDTVs.
+                </p>
             </div>
             <div class="card-container">
                 <a href="<?php echo $gusLink ?>">
@@ -102,11 +115,14 @@ $gusLink = '//gusbarandgrill.com';
     </div>
 </section>
 
-<section class="advanced cover dark">
+<section class="advanced sponsor cover dark">
     <div class="shade">
         <div class="content">
             <div class="text-container">
                 <h3><a href="<?php echo $advancedLink ?>">Advanced Orthopaedics</a></h3>
+                <p>
+                    Advanced Orthopaedics is a full-service orthopaedic practice located in Richmond, VA.  Each patient is treated with the pinpointed expertise of a trained subspecialist, a physician who dedicates his entire practice to one area of orthopaedics. This concentration on a single specialty has resulted in a reputation for excellence—and a long list of satisfied patients.
+                </p>
             </div>
             <div class="card-container">
                 <a href="<?php echo $amazonLink ?>">

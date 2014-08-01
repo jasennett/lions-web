3f2aacbab3bc57545577c00276135926b1a77535
Josh Sennett <josh.sennett@snagajob.com>
2014-07-31 22:57:44 -0400

    Set Porter Cup announcement text

--- application/models/PersistentAnnouncementsModel.php
+++ application/models/PersistentAnnouncementsModel.php
@@ -9,7 +9,8 @@ class PersistentAnnouncementsModel
     {
         $announcement = new PersistentAnnouncement();
         $announcement->title = 'Porter Cup';
-        $announcement->message = 'The Porter Cup is coming up on MM/DD/YYYY.  You\'ll want to sign up at this link.';
+        $announcement->message = 'The 2014 Porter Cup, Richmond\'s XV\'s tournament since 1964, will be held on August 23rd.  ' .
+            'Be sure to register your team <a href="https://squareup.com/market/richmond-lions-rugby-fc/registration">here</a>.';
         $this->announcements = array($announcement);
     }
 } 

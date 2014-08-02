061203f23051c458a927a69425553e201a512395
Josh Sennett <josh.sennett@snagajob.com>
2014-08-01 20:54:27 -0400

    Don't use skrollr for mobile, it seems unstable

--- www/content/js/initskrollr.js
+++ www/content/js/initskrollr.js
@@ -1,6 +1,37 @@
 jQuery(document).ready(function($) {
-    var s = skrollr.init({
+    var initFunction = function()
+    {
+        skrollr.init(
+        {
             forceHeight:false
+        });
+    }
+
+    // initialize skrollr if the window width is large enough
+    if ($(window).width() >= 768)
+    {
+        initFunction();
+    }
+
+    // disable skrollr if the window is resized below 768px wide
+    $(window).on('resize', function ()
+    {
+        if ($(window).width() < 768)
+        {
+            // skrollr.init() returns the singleton created above
+            var s = skrollr.get();
+            if (s)
+            {
+                s.destroy();
+            }
         }
-    );
+        else
+        {
+            var s = skrollr.get();
+            if (!s)
+            {
+                initFunction();
+            }
+        }
+    });
 });
